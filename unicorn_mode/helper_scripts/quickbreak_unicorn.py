31c381e3110853f8d99e49eb0a2618e1df7d7372
jbachell <51917042+jbachell@users.noreply.github.com>
2019-07-02 09:41:22 -0400

    Add files via upload

--- unicorn_mode/helper_scripts/quickbreak_unicorn.py
+++ unicorn_mode/helper_scripts/quickbreak_unicorn.py
@@ -15,17 +15,21 @@ parser.add_argument('--path_to_dumper', help="Looking for unicorn_dumper_gdb.py.
 parser.add_argument('--path_to_gdbserver', help="Looking for gdbserver on phone.  Default will be /data/local/tmp/gdbserver/gdbserver", action="store")
 parser.add_argument('-p', '--port', help="Port to listen to.", action="store")
 parser.add_argument('-d', '--debug', default=False, action="store_true", help="Dump all commandline info.")
+parser.add_argument('--attach', default=False, action="store_true", help="Attach to running program.")
 parser.add_argument('-dr', '--dump-regs', default=False, action="store_true", help="Dump regs at given breakpoint to terminal.")
 parser.add_argument('-y', '--force-yes', default=False, action="store_true", help="Skip the continue (please run once without this option).")
 args = parser.parse_args()
 
 
-print("A few notes:\nPLEASE put program in current directory!!!\n"
-      "I think I did a good job, but program is still in testing phase ... \n... So keep any eye out for bugs\n"
-      "Be careful about names! adb push will delete!\nALSO any UnicornContext* in current directory may delete!\n"
-      "Example usage: my_program 0xffffffff -a arg1 -a arg2\n")
-
 if not args.force_yes:
+    print("\nA few notes:\n\nPLEASE put program in current directory!!! \nThis program will not work without it there!\n"
+          "I think I did a good job, but program is still in testing phase ... \n... So keep any eye out for bugs\n"
+          "Be careful about names! adb push will delete!\nALSO any UnicornContext* in current directory may delete!\n"
+          "-h for full list of arguments, "
+          "-y to avoid this menu\n"
+          "\nExample usage: my_program 0xffffffff -a arg1 -a arg2\n"
+          "               my_program main -a arg1 -a arg2\n")
+
     ans = raw_input("Continue: (y/n): ")
 else:
     ans = 'y'
@@ -33,12 +37,20 @@ else:
 if ans == 'y':
     print("[+] Starting to parse arguments ...")
     failed = False
+    try:
+        print("[*] Breaking at " + str(hex((int(args.breakpoint, 16)))) + " in assembly.")
+        breakFunc = False
+    except:
+        breakFunc = True
+        print("[*] Breaking at function " + args.breakpoint)
     if not args.args_for_program:
         args.args_for_program = ""
         print("[*] No args for program.")
     else:
         args.args_for_program = ' '.join(args.args_for_program)
         print("[*] Args for program are: " + args.args_for_program)
+        if args.attach:
+            print("[-] WARNING! Program arguments are not used in attach mode.")
     if not args.port:
         args.port = "4321"
     print("[*] Port set to: " + args.port)
@@ -64,7 +76,10 @@ if ans == 'y':
 
     instruct.write("source " + args.path_to_gef + "\n")
     instruct.write("target remote :" + args.port + "\n")
-    instruct.write("b *" + args.breakpoint + "\n")
+    if not breakFunc:
+        instruct.write("b *" + args.breakpoint + "\n")
+    else:
+        instruct.write("b " + args.breakpoint + "\n")
     instruct.write("c" + "\n")
     instruct.write("source " + args.path_to_dumper + "\n")
     instruct.write("quit\n")
@@ -75,20 +90,52 @@ if ans == 'y':
         os.system("adb forward tcp:" + args.port + " tcp:" + args.port)
         os.system("adb shell su -c chmod +x /data/local/tmp/" + args.program)
         # os.system("adb shell su -c " + args.path_to_gdbserver + " :" + args.port + " /data/local/tmp/" + args.program + " " + args.args_for_program + " &")
-        os.system("adb shell su -c " + args.path_to_gdbserver + " :" + args.port + " /data/local/tmp/" + args.program + " " + args.args_for_program + "> /dev/null 2>&1 &")
+        if not args.attach:
+            os.system("adb shell su -c " + args.path_to_gdbserver + " :" + args.port + " /data/local/tmp/" + args.program + " " + args.args_for_program + "> /dev/null 2>&1 &")
+        else:
+            # Copied code from below!
+            output__path = "Instructions__attach" + timestamp
+            instruct = open(str(output__path), "w+")
+
+            print("[*] Attaching to PID ... (Can fail if multiple PID's exist)")
+
+            instruct.write("su" + "\n")
+            instruct.write("pidof " + args.program + "> data/local/tmp/pid_lolhere.txt\n")
+            instruct.write(args.path_to_gdbserver + " --attach :" + args.port + " `cat /data/local/tmp/pid_lolhere.txt`" + " > /dev/null 2>&1 &" + "\n")
+            instruct.write("rm " + "/data/local/tmp/pid_lolhere.txt\n")
+            instruct.close()
+
+            os.system("adb shell < " + output__path)
         os.system("arm-linux-gnueabi-gdb " + args.program + " < " + output_path)
         # subprocess.check_output("adb shell su -c rm /data/local/tmp/" + args.program, shell=True)
         # subprocess.check_output("adb forward --remove-all", shell=True)
         # subprocess.check_output("rm " + output_path, shell=True)
+        print("\n\nIf --attach was used, other errors might be possible.  Check instructions__attach in your current directory.")
         exit(0)
 
     if not failed:
         try:
-            subprocess.check_output("adb push " + args.program + " /data/local/tmp", shell=True)
-            print("[+] Pushed program to /data/local/tmp")
             subprocess.check_output("adb forward tcp:" + args.port + " tcp:" + args.port, shell=True)
-            os.system("adb shell su -c chmod +x /data/local/tmp/" + args.program + "> /dev/null 2>&1")
-            os.system("adb shell su -c " + args.path_to_gdbserver + " :" + args.port + " /data/local/tmp/" + args.program + " " + args.args_for_program + "> /dev/null 2>&1 &")
+            if not args.attach:
+                subprocess.check_output("adb push " + args.program + " /data/local/tmp", shell=True)
+                print("[+] Pushed program to /data/local/tmp")
+                os.system("adb shell su -c chmod +x /data/local/tmp/" + args.program + "> /dev/null 2>&1")
+                os.system("adb shell su -c " + args.path_to_gdbserver + " :" + args.port + " /data/local/tmp/" + args.program + " " + args.args_for_program + "> /dev/null 2>&1 &")
+            else:
+                output__path = "Instructions__attach" + timestamp
+                instruct = open(str(output__path), "w+")
+
+                print("[*] Attaching to PID ...")
+
+                instruct.write("su" + "\n")
+                instruct.write("pidof " + args.program + "> data/local/tmp/pid_lolhere.txt\n")
+                instruct.write(args.path_to_gdbserver + " --attach :" + args.port + " `cat /data/local/tmp/pid_lolhere.txt`" + " > /dev/null 2>&1 &" + "\n")
+                instruct.write("rm " + "/data/local/tmp/pid_lolhere.txt\n")
+                instruct.close()
+
+                os.system("adb shell < " + output__path)
+
+                print("[+] Attached successfully!")
             print("[+] Started listener")
         except:
             print("[-] Something went wrong with ADB, is your device connected? Is the program path right?")
@@ -100,7 +147,7 @@ if ans == 'y':
             print("[*] Any output from program's STDERR should show below ...")
             os.system("arm-linux-gnueabi-gdb " + args.program + " < " + output_path + "> /dev/null 2>&1")
         except:
-            print("[-] Something went wrong with GDB ... either GDB isn't here, breakpoint didn't work, or the paths for the dumper/gef are off.")
+            print("[-] Something went wrong with GDB ... either GDB isn't here, breakpoint didn't work, or the paths for the dumper/gef are off.  Trying '-d' might help.")
             failed = True
 
     output_path1 = args.program + "_" + args.breakpoint
@@ -109,25 +156,29 @@ if ans == 'y':
         print("[+] GDB finished!")
 
     if not failed:
-        os.system("cp -r UnicornContext* " + "herelolol")
-        if args.dump_regs:
-            print("[*] Preparing to print registers ...")
-            try:
-                index_file_path = os.path.join(os.getcwd() + "/herelolol", "_index.json")
-                if not os.path.isfile(index_file_path):
-                    raise Exception("Index file not found. Expected it to be at {}".format(index_file_path))
-                index_file = open(index_file_path, 'r')
-                context = json.load(index_file)
-                print("Dump at breakpoint " + args.breakpoint + ":")
-                for reg, value in sorted (context['regs'].iteritems()):
-                    print(">>> {0:>4}: 0x{1:016x}".format(reg, value))
-                index_file.close()
-                print("[+] Registers dumped!")
-            except:
-                print("[-] Reading registers failed. Is json installed?")
-        if len(os.listdir(os.getcwd() + '/herelolol')) == 0:
-            print("[-] Unicorn failed to dump.  Try the '-d' option.  Maybe the listener isn't up?")
-            subprocess.check_output("rm -r herelolol UnicornContext*", shell=True)
+        try:
+            os.system("cp -r UnicornContext* " + "herelolol")
+            if args.dump_regs:
+                print("[*] Preparing to print registers ...")
+                try:
+                    index_file_path = os.path.join(os.getcwd() + "/herelolol", "_index.json")
+                    if not os.path.isfile(index_file_path):
+                        raise Exception("Index file not found. Expected it to be at {}".format(index_file_path))
+                    index_file = open(index_file_path, 'r')
+                    context = json.load(index_file)
+                    print("Dump at breakpoint " + args.breakpoint + ":")
+                    for reg, value in sorted (context['regs'].iteritems()):
+                        print(">>> {0:>4}: 0x{1:016x}".format(reg, value))
+                    index_file.close()
+                    print("[+] Registers dumped!")
+                except:
+                    print("[-] Reading registers failed. Is json installed?")
+            if len(os.listdir(os.getcwd() + '/herelolol')) == 0:
+                print("[-] Unicorn failed to dump.  Try the '-d' option.  Maybe the listener isn't up?")
+                subprocess.check_output("rm -r herelolol UnicornContext*", shell=True)
+                failed = True
+        except:
+            print("[-] Missing directory, something went wrong.  ")
             failed = True
 
     print("[*] Cleaning up :)")
@@ -135,9 +186,10 @@ if ans == 'y':
         subprocess.check_output("adb shell su -c rm /data/local/tmp/" + args.program, shell=True)
         subprocess.check_output("adb forward --remove-all", shell=True)
         subprocess.check_output("rm " + output_path, shell=True)
+        if args.attach:
+            subprocess.check_output("rm " + output__path, shell=True)
     except:
         print("[-] Clean up failed.")
-
 
     if not failed:
         if not os.path.exists(output_path1):
