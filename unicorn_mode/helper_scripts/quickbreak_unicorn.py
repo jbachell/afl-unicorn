7b7766f5faff9df421daf607ef4f8e878ac37ffa
jbachell <51917042+jbachell@users.noreply.github.com>
2019-07-03 11:42:30 -0400

    Add files via upload

--- unicorn_mode/helper_scripts/quickbreak_unicorn.py
+++ unicorn_mode/helper_scripts/quickbreak_unicorn.py
@@ -9,23 +9,27 @@ import json
 parser = argparse.ArgumentParser()
 parser.add_argument('program', type=str, help="Path to program.  MUST be in current directory!")
 parser.add_argument('-a', '--args_for_program', help="Arguments for program to run with.  Default is none.", action="append")
-parser.add_argument('breakpoint', type=str, help="Breakpoint to start dump at, use only hex string, including 0x")
-parser.add_argument('--path_to_gef', help="Looking for gef.py.  Default will be current directory.", action="store")
-parser.add_argument('--path_to_dumper', help="Looking for unicorn_dumper_gdb.py.  Default will be current directory.", action="store")
-parser.add_argument('--path_to_gdbserver', help="Looking for gdbserver on phone.  Default will be /data/local/tmp/gdbserver/gdbserver", action="store")
+parser.add_argument('breakpoint', type=str, help="Breakpoint to start dump at, use hex string or function name, include 0x if hex string")
+parser.add_argument('-gef', '--path-to-gef', help="Looking for gef.py.  Default will be current directory.", action="store")
+parser.add_argument('-dump', '--path-to-dumper', help="Looking for unicorn_dumper_gdb.py.  Default will be current directory.", action="store")
+parser.add_argument('-gdb', '--path-to-gdbserver', help="Looking for gdbserver on phone.  Default will be /data/local/tmp/gdbserver/gdbserver", action="store")
 parser.add_argument('-p', '--port', help="Port to listen to.", action="store")
-parser.add_argument('-d', '--debug', default=False, action="store_true", help="Dump all commandline info.")
-parser.add_argument('--attach', default=False, action="store_true", help="Attach to running program.")
-parser.add_argument('-dr', '--dump-regs', default=False, action="store_true", help="Dump regs at given breakpoint to terminal.")
+parser.add_argument('--attach', default=False, action="store_true", help="Attach to running process on device. Program name required (not PID).")
+parser.add_argument('-dr', '--dump-regs', default=False, action="store_true", help="Dump regs at current breakpoint to terminal.")
 parser.add_argument('-y', '--force-yes', default=False, action="store_true", help="Skip the continue and intro (please run once without this option).")
+parser.add_argument('-n', '--new-folder-name', help="Set the name of the new folder being made.  Default will be program-name_breakpoint", action="store")
+parser.add_argument('-d', '--debug', default=False, action="store_true", help="Dump all commandline info with no cleanup.")
 args = parser.parse_args()
 
 
 if not args.force_yes:
-    print("\nA few notes:\n\nPLEASE put program in current directory!!! \nThis program will not work without it there!\n"
+    print("\nA few notes:\n\nIMPORTANT: PLEASE put program in current directory!!! \nThis program will not work without it there!\n"
           "I think I did a good job, but program is still in testing phase ... \n... So keep any eye out for bugs\n"
           "Be careful about names! adb push will delete!\nALSO any UnicornContext* in current directory may delete!\n"
-          "-h for full list of arguments, "
+          "This is sorta a dumb program, just simulating commands.\n"
+          "Errors should (hopefully) always be thrown ...\n...But the program doesn't acutally know what ran and didn't run ...\n..."
+          "i.e. The program says GDB ran but it didn't actually run.\n"
+          "-h for full list of arguments (there are a LOT of options)\n"
           "-y to avoid this menu\n"
           "\nExample usage: my_program 0xffffffff -a arg1 -a arg2\n"
           "               my_program main -a arg1 -a arg2\n")
@@ -69,6 +73,11 @@ if ans == 'y':
         print("[*] Path for gdbserver is /data/local/tmp/gdbserver/gdbserver")
     else:
         print("[*] Path for gdbserver is " + args.path_to_gdbserver)
+    if not args.new_folder_name:
+        args.new_folder_name = args.program + "_" + args.breakpoint
+        print("[*] Dump folder name is " + args.new_folder_name)
+    else:
+        print("[*] Dump folder name is " + args.new_folder_name)
 
     timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y%m%d')
     output_path = "Instructions_" + timestamp
@@ -86,6 +95,7 @@ if ans == 'y':
     instruct.close()
 
     if args.debug:
+        print("[-] WARNING! Some functionalities of debug might not work.")
         os.system("adb push " + args.program + " /data/local/tmp")
         os.system("adb forward tcp:" + args.port + " tcp:" + args.port)
         os.system("adb shell su -c chmod +x /data/local/tmp/" + args.program)
@@ -110,7 +120,7 @@ if ans == 'y':
         # subprocess.check_output("adb shell su -c rm /data/local/tmp/" + args.program, shell=True)
         # subprocess.check_output("adb forward --remove-all", shell=True)
         # subprocess.check_output("rm " + output_path, shell=True)
-        print("\n\nIf --attach was used, other errors might be possible.  Check instructions__attach in your current directory.")
+        print("\n\n[*] If --attach was used, other errors might be possible.  Check instructions__attach in your current directory.")
         exit(0)
 
     if not failed:
@@ -144,24 +154,24 @@ if ans == 'y':
     print("[*] Runing GDB ...")
     if not failed:
         try:
-            print("[*] Any output from program's STDERR should show below ...")
+            print("[*] Some useful output from program might show below ...")
             os.system("arm-linux-gnueabi-gdb " + args.program + " < " + output_path + "> /dev/null 2>&1")
         except:
             print("[-] Something went wrong with GDB ... either GDB isn't here, breakpoint didn't work, or the paths for the dumper/gef are off.  Trying '-d' might help.")
             failed = True
 
-    output_path1 = args.program + "_" + args.breakpoint
+    output_path1 = args.new_folder_name
 
     if not failed:
         print("[+] GDB finished!")
 
     if not failed:
         try:
-            os.system("cp -r UnicornContext* " + "herelolol")
+            os.system("cp -r UnicornContext* " + "here...iguess")
             if args.dump_regs:
                 print("[*] Preparing to print registers ...")
                 try:
-                    index_file_path = os.path.join(os.getcwd() + "/herelolol", "_index.json")
+                    index_file_path = os.path.join(os.getcwd() + "/here...iguess", "_index.json")
                     if not os.path.isfile(index_file_path):
                         print("[-] Something went wrong, the json index file wasn't found.")
                         failed = True
@@ -174,12 +184,12 @@ if ans == 'y':
                     print("[+] Registers dumped!")
                 except:
                     print("[-] Reading registers failed. Is json installed?")
-            if len(os.listdir(os.getcwd() + '/herelolol')) == 0:
+            if len(os.listdir(os.getcwd() + '/here...iguess')) == 0:
                 print("[-] Unicorn failed to dump.  Try the '-d' option.  Maybe the listener isn't up?")
-                subprocess.check_output("rm -r herelolol UnicornContext*", shell=True)
+                subprocess.check_output("rm -r here...iguess UnicornContext*", shell=True)
                 failed = True
         except:
-            print("[-] Missing directory, something went wrong.  ")
+            print("[-] Missing directory, something went wrong.  Were the paths correct?")
             failed = True
 
     print("[*] Cleaning up :)")
@@ -195,9 +205,9 @@ if ans == 'y':
     if not failed:
         if not os.path.exists(output_path1):
               os.makedirs(output_path1)
-        os.system("mv UnicornContext* " + args.program + "_" + args.breakpoint)
-        subprocess.check_output("rm -r herelolol", shell=True)
-        print("[+] All set! Look for new directory named " + args.program + "_" + args.breakpoint)
+        os.system("mv UnicornContext* " + args.new_folder_name)
+        subprocess.check_output("rm -r here...iguess", shell=True)
+        print("[+] All set! Look for new directory named " + args.new_folder_name)
 
     if failed:
         print("[-] Failed")
